7992cfb06029b114873ed4b253e5320362019c93
James Burgess <james.burgess@login-01.clusternet>
2023-12-12 08:50:15 -0800

    .

--- dataset.py
+++ dataset.py
@@ -10,204 +10,234 @@ from pathlib import Path
 import tqdm
 
 class OpencellPPI(InMemoryDataset):
-	file_metadata = "opencell-library-metadata.csv"
-	file_interactions = "opencell-protein-interactions.csv"
-	file_interactions_readme = "opencell-protein-interactions-readme.csv"
-	file_localization_annotation = "opencell-localization-annotations.csv"
-	file_localization_annotation_readme = "opencell-localization-annotations-readme.csv"
-	raw_files = [
-		file_metadata,
-		file_interactions, file_interactions_readme, 
-		file_localization_annotation,  file_localization_annotation_readme
-		]
-	url_base = "https://opencell.czbiohub.org/data/datasets/"
+    file_metadata = "opencell-library-metadata.csv"
+    file_interactions = "opencell-protein-interactions.csv"
+    file_interactions_readme = "opencell-protein-interactions-readme.csv"
+    file_localization_annotation = "opencell-localization-annotations.csv"
+    file_localization_annotation_readme = "opencell-localization-annotations-readme.csv"
+    raw_files = [
+        file_metadata, file_interactions, file_interactions_readme,
+        file_localization_annotation, file_localization_annotation_readme
+    ]
+    url_base = "https://opencell.czbiohub.org/data/datasets/"
 
-	processed_files = ["processed.pt"]
+    processed_files = ["processed.pt"]
 
-	def __init__(self,
-				 root: str,
-				 transform: Optional[Callable] = None,
-				 pre_transform: Optional[Callable] = None,
-				 pre_filter: Optional[Callable] = None,
-				 features_type="dummy",
-				 test_split_method="cite_order",
-				 test_split_frac=0.2,
-				 ):
-		"""
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        pre_transform: Optional[Callable] = None,
+        pre_filter: Optional[Callable] = None,
+        features_type="dummy",
+        test_split_method="cite_order",
+        test_split_frac=0.2,
+    ):
+        """
 		test_split_method: ("cite_order","random")
 		features: ("dummy","sequencelanguage","image","sequencelanguage_image")
 		"""
-		self.test_split_method = test_split_method
-		self.test_split_frac = test_split_frac
-		self.features_type = features_type
-		super().__init__(root, transform, pre_transform, pre_filter)
-		self.load(self.processed_paths[0])
+        self.test_split_method = test_split_method
+        self.test_split_frac = test_split_frac
+        self.features_type = features_type
+        super().__init__(root, transform, pre_transform, pre_filter)
 
-	def download(self):
-		# data dir
-		data_dir = Path(self.root)
-		data_dir.mkdir(exist_ok=True)
-		headers = {'Referer': self.url_base}
+        self.load(self.processed_paths[0])
 
-		# do HTTP request for the OpenCell data
-		for file in self.raw_files:
-			response = requests.get(f"{self.url_base}/{file}", headers=headers)
-			if response.status_code == 200:
-				file_path = data_dir / "raw" / file
-				with open(file_path, 'wb') as f:
-					f.write(response.content)
-				print(f"Downloaded {file_path}")
-			else:
-				raise (
-					f"Failed to download {file_path} with status code {response.status_code}"
-				)
+        if self.features_type == "dummy":
+            self._data.x = self._data.dummy_features
+        elif self.features_type == "sequencelanguage":
+        	self._data.x = self._data.features_sequence 
+        elif self.features_type == "image":
+        	self._data.x = self._data.features_image
+        elif self.features_type == "sequencelanguage_image":
+        	raise NotImplementedError("define fusion")
+        pass
 
-	@property
-	def raw_file_names(self):
-		return self.raw_files 
+    def download(self):
+        # data dir
+        data_dir = Path(self.root)
+        data_dir.mkdir(exist_ok=True)
+        headers = {'Referer': self.url_base}
 
-	@property
-	def processed_file_names(self):
-		return self.processed_files
+        # do HTTP request for the OpenCell data
+        for file in self.raw_files:
+            response = requests.get(f"{self.url_base}/{file}", headers=headers)
+            if response.status_code == 200:
+                file_path = data_dir / "raw" / file
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+                print(f"Downloaded {file_path}")
+            else:
+                raise (
+                    f"Failed to download {file_path} with status code {response.status_code}"
+                )
 
-	def process(self):
+    @property
+    def raw_file_names(self):
+        return self.raw_files
 
-		# start with annotations for protein localization - defines protein indexing
-		fname_localization = f"{self.root}/raw/{self.file_localization_annotation}"
-		df_loc = pd.read_csv(fname_localization, na_filter=False)
-		self.prots = df_loc['target_name'].values
-		self.prot_to_idx = dict(zip(self.prots, range(len(self.prots))))
-		self.idx_to_prot = {v:k for k,v in self.prot_to_idx.items()}
+    @property
+    def processed_file_names(self):
+        return self.processed_files
 
-		# labels for subcellular localization
-		self.y_loc_nuclear, self.y_loc = self.get_node_labels_localization(df_loc)
+    def process(self):
+        # start with annotations for protein localization - defines protein indexing
+        fname_localization = f"{self.root}/raw/{self.file_localization_annotation}"
+        df_loc = pd.read_csv(fname_localization, na_filter=False)
+        self.prots = df_loc['target_name'].values
+        self.prot_to_idx = dict(zip(self.prots, range(len(self.prots))))
+        self.idx_to_prot = {v: k for k, v in self.prot_to_idx.items()}
 
-		# get the features
-		if self.features_type=="dummy":
-			self.features = torch.ones((len(self.prots),5))
+        # labels for subcellular localization
+        self.y_loc_nuclear, self.y_loc = self.get_node_labels_localization(
+            df_loc)
 
-		else:
-			raise NotImplementedError()
-			# other options are ("dummy","sequencelanguage","image","sequencelanguage_image") 
-			self.features_sequence = self.get_features_sequence()
-			self.features_image = self.get_features_image()
+        # get the features
+        self.dummy_features = torch.ones((len(self.prots), 5))
+        self.features_sequence = self.get_features_sequence()
+        self.features_image = self.get_features_image()
 
-		# get edges: the interaction data 
-		df_interact = pd.read_csv(f"{self.root}/raw/{self.file_interactions}")
-		edge_lst = []
-		for i, row in df_interact.iterrows():
-			prot0, prot1 = row['target_gene_name'], row['interactor_gene_name']
-			if ((prot0 in self.prots) and (prot1 in self.prots)):
-				edge_lst.append([self.prot_to_idx[prot0], self.prot_to_idx[prot1]])
-		self.edge_index = torch.from_numpy(np.stack(edge_lst)).swapaxes(1,0)
+        # get edges: the interaction data
+        df_interact = pd.read_csv(f"{self.root}/raw/{self.file_interactions}")
+        edge_lst = []
+        for i, row in df_interact.iterrows():
+            prot0, prot1 = row['target_gene_name'], row['interactor_gene_name']
+            if ((prot0 in self.prots) and (prot1 in self.prots)):
+                edge_lst.append(
+                    [self.prot_to_idx[prot0], self.prot_to_idx[prot1]])
+        self.edge_index = torch.from_numpy(np.stack(edge_lst)).swapaxes(1, 0)
 
-		# generate train mask. Random or by ordering the proteins by pubmed cites
-		if self.test_split_method == "random":
-			idxs = torch.randperm(len(self.prots))
-			idxs_test = idxs[:int(self.test_split_frac*len(self.prots))]
+        # generate train mask. Random or by ordering the proteins by pubmed cites
+        if self.test_split_method == "random":
+            idxs = torch.randperm(len(self.prots))
+            idxs_test = idxs[:int(self.test_split_frac * len(self.prots))]
 
-		elif self.test_split_method == "cite_order":
-			self.pubmed_cite_order_norm = self.get_protein_ordering()
-			idxs_test = torch.where(self.pubmed_cites_order_norm<=self.test_split_frac)[0]
+        elif self.test_split_method == "cite_order":
+            self.pubmed_cite_order_norm = self.get_protein_ordering()
+            idxs_test = torch.where(
+                self.pubmed_cites_order_norm <= self.test_split_frac)[0]
 
-		else:
-			raise ValueError("test_split_method must be one of ()'cite_order','random")
+        else:
+            raise ValueError(
+                "test_split_method must be one of ()'cite_order','random")
 
-		self.train_mask = torch.ones(len(self.prots))
-		self.train_mask[idxs_test] = 0
-		self.test_mask = 1-self.train_mask
+        self.train_mask = torch.ones(len(self.prots))
+        self.train_mask[idxs_test] = 0
+        self.test_mask = 1 - self.train_mask
 
-		# create a list of data objects 
-		data_list = [Data(x=self.features, edge_index=self.edge_index, 
-			train_mask=self.train_mask, test_mask=self.test_mask, 
-			y_loc_nuclear=self.y_loc_nuclear, y_loc=self.y_loc,
-			)]
+        # create a list of data objects
+        data_list = [
+            Data(x=self.dummy_features,
+                 dummy_features=self.dummy_features,
+                 features_sequence=self.features_sequence,
+                 features_image=self.features_image,
+                 edge_index=self.edge_index,
+                 train_mask=self.train_mask,
+                 test_mask=self.test_mask,
+                 y_loc_nuclear=self.y_loc_nuclear,
+                 y_loc=self.y_loc,
+                 id_to_loc=self.id_to_loc)
+        ]
 
-		# # boilerplate stuff  
-		# if self.pre_filter is not None:
-		# 	data_list = [data for data in data_list if self.pre_filter(data)]
 
-		# if self.pre_transform is not None:
-		# 	data_list = [self.pre_transform(data) for data in data_list]
-		
-		self.save(data_list, self.processed_paths[0])
+        # # boilerplate stuff
+        # if self.pre_filter is not None:
+        # 	data_list = [data for data in data_list if self.pre_filter(data)]
 
-	def get_node_labels_localization(self, df_loc):
-		""" 
+        # if self.pre_transform is not None:
+        # 	data_list = [self.pre_transform(data) for data in data_list]
+
+        self.save(data_list, self.processed_paths[0])
+
+    def get_node_labels_localization(self, df_loc, threshold_include=40):
+        """ 
 		Read the subcellular localization labels. Generate two types of labels:
-			
+
 			y_loc_nuclear: 1 if the protein localizes to the nucleus, and 0 
 			otherwise. If it localizes both in and outside the nulceus, then 1.
 			The shape is (n_proteins,1)
-			
+
 			y_loc: multiclass classification. Protein can localize to multiple 
 			compartments. If there are `n_classes` number of classes, then the 
 			output shape is (n_proteins,n_classes), where each entry is 0 or 1.
 		"""
-		loc_grade3 = df_loc['annotations_grade_3']
-		# get labels for the binary classification task
-		y_loc_nuclear = torch.tensor(['nucle' in s for s in loc_grade3])
-		y_loc_nuclear = y_loc_nuclear.long().unsqueeze(1)
+        loc_grade3 = df_loc['annotations_grade_3']
+        # get labels for the binary classification task
+        y_loc_nuclear = torch.tensor(['nucle' in s for s in loc_grade3])
+        y_loc_nuclear = y_loc_nuclear.long().unsqueeze(1)
 
-		# get the unique localization labels
-		all_locs = [s.split(";") for s in loc_grade3]
-		all_locs_flat = [s for s_ in all_locs for s in s_] 	# flatten the list
-		uniq_locs = np.sort(np.unique(all_locs_flat))		# get unique stuff
-		self.uniq_locs = [s for s in uniq_locs if s!='']# remove empty entries
-		self.loc_to_id = dict(zip(self.uniq_locs, range(len(self.uniq_locs))))
-		self.id_to_loc = {v:k for k,v in self.loc_to_id.items()}
+        # get the unique localization labels
+        all_locs = [s.split(";") for s in loc_grade3]
+        all_locs_flat = [s for s_ in all_locs for s in s_]  # flatten the list
+        uniq_locs, uniq_cnts = np.unique(all_locs_flat, return_counts=True)
+        uniq_locs = uniq_locs[uniq_cnts >= threshold_include]
+        uniq_locs = np.sort(uniq_locs)  # get unique stuff
+        self.uniq_locs = [s for s in uniq_locs
+                          if s != '']  # remove empty entries
+        self.loc_to_id = dict(zip(self.uniq_locs, range(len(self.uniq_locs))))
+        self.id_to_loc = {v: k for k, v in self.loc_to_id.items()}
 
-		# get labels for the multiclass classification task
-		n_proteins = len(loc_grade3)
-		n_classes = len(self.uniq_locs)
-		y_loc = torch.zeros((n_proteins, n_classes))
-		for i, locs in enumerate(all_locs):
-			for _, loc in enumerate(locs):
-				if loc == "": 
-					continue
-				y_loc[i, self.loc_to_id[loc]] = 1
+        # get labels for the multiclass classification task
+        n_proteins = len(loc_grade3)
+        n_classes = len(self.uniq_locs)
+        y_loc = torch.zeros((n_proteins, n_classes))
+        for i, locs in enumerate(all_locs):
+            for _, loc in enumerate(locs):
+                if loc not in uniq_locs:
+                    continue
+                y_loc[i, self.loc_to_id[loc]] = 1
 
-		return y_loc_nuclear, y_loc
-	
-	def get_features_sequence(self):
-		""" 
+        return y_loc_nuclear, y_loc
+
+    def get_features_sequence(self):
+        """ 
 		From file, generate a list of sequences and an array of representations 
 		derived from a protein language model. 
 		"""
-		
-		file_protein_sequences = f"{self.root}/raw/protein_sequences.pt"
-		lookup_protein_sequences = torch.load(file_protein_sequences)
-		self.sequences = [lookup_protein_sequences.get(p,"") for p in self.prots]
-		
-		file_sequence_representations = f"{self.root}/raw/sequence_representations.pt"
-		self.sequence_representations = torch.load(file_sequence_representations)
+        file_protein_sequences = f"{self.root}/raw/protein_sequences.pt"
+        lookup_protein_sequences = torch.load(file_protein_sequences)
+        self.sequences = [
+            lookup_protein_sequences.get(p, "") for p in self.prots
+        ]
 
-	def get_features_image(self):
-		""" """
-		pass 
+        file_sequence_representations = f"{self.root}/raw/sequence_representations.pt"
+        self.sequence_representations = torch.load(
+            file_sequence_representations)
 
-	def get_protein_ordering(self):
-		""" 
+        return self.sequence_representations
+
+    def get_features_image(self):
+        """ """
+        file_image_representations = f"{self.root}/raw/opencell_image_embedding.pt"
+        self.image_representations = torch.load(
+            file_image_representations)
+        return self.image_representations
+
+    def get_protein_ordering(self):
+        """ 
 		Assign a rank ordering, and a normalized rank orderings to each of protein.
-		This is based on data that we import that was previously calculated in 
-		the `dataset_preprocessing.py` func. 
+		This is based on citation counds,  that we import that was previously 
+		calculated in the `dataset_preprocessing.py` func. 
 		"""
-		file_pubmed_cites = f"{self.root}/raw/prot_pubmed_counts.pt"
-		lookup_pubmed_cites = torch.load(file_pubmed_cites)
-		self.pubmed_cites = torch.tensor([lookup_pubmed_cites[p] for p in self.prots])
-		# get the rank ordering 
-		sorted_indices = sorted(range(len(self.pubmed_cites)), 
-			key=lambda x: self.pubmed_cites[x])
-		self.pubmed_cites_order = torch.zeros(len(sorted_indices))
-		for rank, index in enumerate(sorted_indices):
-			self.pubmed_cites_order[index] = rank
+        file_pubmed_cites = f"{self.root}/raw/prot_pubmed_counts.pt"
+        lookup_pubmed_cites = torch.load(file_pubmed_cites)
+        self.pubmed_cites = torch.tensor(
+            [lookup_pubmed_cites[p] for p in self.prots])
+        # get the rank ordering
+        sorted_indices = sorted(range(len(self.pubmed_cites)),
+                                key=lambda x: self.pubmed_cites[x])
+        self.pubmed_cites_order = torch.zeros(len(sorted_indices))
+        for rank, index in enumerate(sorted_indices):
+            self.pubmed_cites_order[index] = rank
 
-		self.pubmed_cites_order_norm = self.pubmed_cites_order / self.pubmed_cites_order.max()
+        self.pubmed_cites_order_norm = self.pubmed_cites_order / self.pubmed_cites_order.max(
+        )
 
-		return self.pubmed_cites_order_norm
+        return self.pubmed_cites_order_norm
 
 
 if __name__ == "__main__":
-	dataset = OpencellPPI(root="data")
-	ipdb.set_trace()
-	pass
+    dataset = OpencellPPI(root="data")
+    ipdb.set_trace()
+    pass
--- train_node_preds.py
+++ train_node_preds.py
@@ -1,54 +1,241 @@
 from dataset import OpencellPPI
 from models import gcn
 import torch
-import ipdb 
+import ipdb
+from sklearn.metrics import f1_score
+from utils import FocalLoss
+from typing import Literal
 
-def train(model, data, train_idx, optimizer, loss_fn):
-	""" train one step of a PYG graph model """
-	model.train()
-	loss = 0
-	optimizer.zero_grad()
-	pred_logit = model(data.x, data.edge_index)
-	pred_logit_train, label_train = pred_logit[train_idx], data.y[train_idx]
-	loss = loss_fn(pred_logit_train, label_train.float())
-	loss.backward()
-	optimizer.step()
-	return loss.item()
 
-def eval(model, data, test_idx):
-	pred_logit = model(data.x, data.edge_index)
-	pred_logit_test, label_test = pred_logit[test_idx], data.y[test_idx]
-	pred_test = torch.sigmoid(pred_logit_test)
-	pred_test = (pred_test>=0.5).int()
+def train_step(model,
+               data,
+               train_idx,
+               optimizer,
+               loss_fn,
+               is_graph_model=True):
+    """ train one step of a PYG graph model """
+    model.train()
+    loss = 0
+    optimizer.zero_grad()
+    if is_graph_model:
+        pred_logit = model(data.x, data.edge_index)
+    else:
+        pred_logit = model(data.x)
+    pred_logit_train, label_train = pred_logit[train_idx], data.y[train_idx]
+    loss = loss_fn(pred_logit_train, label_train.float())
+    loss.backward()
+    optimizer.step()
+    return loss.item()
 
-	acc = (pred_test==label_test).sum() / len(pred_test)
-	return acc.item()
 
-if __name__=="__main__":
-	dataset = OpencellPPI(root="data", features_type="dummy", 
-		test_split_method="cite_order", test_split_frac=0.2)
-	data = dataset[0]
-	data.y = data.y_loc_nuclear
-	data = data.cuda()
+def eval(model, data, test_idx, is_graph_model=True):
+    """
+	Differennt implementation for binary vs multilabel classification
+	"""
+    if is_graph_model:
+        pred_logit = model(data.x, data.edge_index)
+    else:
+        pred_logit = model(data.x)
+    pred_logit_test, label_test = pred_logit[test_idx], data.y[test_idx]
+    pred_test = torch.sigmoid(pred_logit_test)
+    pred_test = (pred_test >= 0.5).int()
+    label_test, pred_test = label_test.detach().cpu().numpy(
+    ), pred_test.detach().cpu().numpy()
 
-	model = gcn.GCN(input_dim=data.x.shape[1], hidden_dim=64, output_dim=1, 
-		num_layers=3, dropout=0.2)
-	model.reset_parameters()
-	model.cuda()
+    # case 1: binary classification - single class
+    if pred_logit.shape[1] == 1:
+        acc = (pred_test == label_test).sum() / len(pred_test)  # float
+        acc_mean = acc
+        f1 = f1_score(label_test, pred_test)
+        f1_mean = f1
 
-	optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-	loss_fn = torch.nn.BCEWithLogitsLoss()
-	train_idx = torch.where(data.train_mask)[0]
-	test_idx = torch.where(data.test_mask)[0]
+    else:
+        acc = (pred_test == label_test).sum(0) / len(pred_test)  # is an array
+        acc_mean = acc.mean()  # balanced accuracy
+        f1 = f1_score(label_test, pred_test, average=None)
+        f1_mean = f1.mean()
 
-	acc_all, loss_all = [], []
-	for i in range(10000):
-		loss_scal = train(model, data, train_idx, optimizer, loss_fn)
-		if i%100 == 0:
-			acc = eval(model, data, test_idx)
-			acc_all.append(acc)
-			loss_all.append(loss_scal)
-			print(acc)
+    return acc, acc_mean, f1, f1_mean
 
-	ipdb.set_trace()
-	pass
+
+def get_model_from_name(model_name: Literal["gcn", "linear"],
+                        data,
+                        hyperparam_key=None):
+    if model_name == "gcn":
+        model = gcn.GCN(input_dim=data.x.shape[1],
+                        hidden_dim=128,
+                        output_dim=data.y.shape[1],
+                        num_layers=3,
+                        dropout=0.2)
+
+    elif model_name == "linear":
+        model = torch.nn.Linear(data.x.shape[1], data.y.shape[1])
+
+    else:
+        raise NotImplementedError()
+    return model
+
+
+def do_training_0(model_name='gcn',
+                  test_split_frac=0.2,
+                  test_split_method="cite_order",
+                  features_type='dummy',
+                  n_steps=10000):
+    """
+	First experiment is the most basic. 
+	The task is a simple binary classification for whether therte 
+	This label is in the dataset attribute `y_loc_nuclear`. 
+	"""
+    dataset = OpencellPPI(root="data",
+                          features_type=features_type,
+                          test_split_method=test_split_method,
+                          test_split_frac=test_split_frac)
+    print(
+        "Training problem 0: binary classification of whether there is nuclear organelles"
+    )
+    print(f"Dataset attributes, test_split_method={test_split_method}, " \
+     f"test_split_frac={test_split_frac}, features_type={features_type}")
+    data = dataset[0]
+    data.y = data.y_loc_nuclear
+    output_dim = data.y.shape[1]
+    data = data.cuda()
+    model = get_model_from_name(model_name, data)
+    is_graph_model = False if model_name == 'linear' else True
+    model.reset_parameters()
+    model.cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    train_idx = torch.where(data.train_mask)[0]
+    test_idx = torch.where(data.test_mask)[0]
+
+    # get the baseline accuracy and print it
+    labels_test = data.y[test_idx]
+    labels_majority_classs = (labels_test.sum() / len(labels_test)).item()
+    labels_majority_classs = max(labels_majority_classs,
+                                 1 - labels_majority_classs)
+    print(
+        f"Accuracy of choosing the majority class {100*labels_majority_classs:.1f}%"
+    )
+
+    acc_all, loss_all = [], []
+    best_acc, best_f1 = 0, 0
+    for i in range(n_steps):
+        loss_scal = train_step(model,
+                               data,
+                               train_idx,
+                               optimizer,
+                               loss_fn,
+                               is_graph_model=is_graph_model)
+        if i % 100 == 0:
+            acc, acc_mean, f1, f1_mean = eval(model,
+                                              data,
+                                              test_idx,
+                                              is_graph_model=is_graph_model)
+            best_acc = max(acc, best_acc)
+            best_f1 = max(f1, best_f1)
+            acc_all.append(acc)
+            loss_all.append(loss_scal)
+            print(f"\rCurrent accuracy={100*acc:.1f}%, best_accuracy={100*best_acc:.1f}%, " \
+             f"f1={f1:.2f}, best f1={best_f1:.2f}", end='', flush=True) # print with return carriage
+    print()
+    print(f"Best accuracy {100*best_acc:.1f}%")
+
+
+def do_training_1(model='gcn',
+                  test_split_frac=0.2,
+                  test_split_method="cite_order",
+                  features_type='dummy',
+                  n_steps=10000):
+    """
+	Multi-class classification for the 
+	This label is in the dataset attribute `y_loc_nuclear`. 
+	"""
+    dataset = OpencellPPI(root="data",
+                          features_type=features_type,
+                          test_split_method=test_split_method,
+                          test_split_frac=test_split_frac)
+    print(
+        "Training problem 0: binary classification of whether there is nuclear organelles"
+    )
+    print(f"Dataset attributes, test_split_method={test_split_method}, " \
+     f"test_split_frac={test_split_frac}, features_type={features_type}")
+    data = dataset[0]
+    data.y = data.y_loc
+    data = data.cuda()
+
+    output_dim = data.y.shape[1]
+    model = gcn.GCN(input_dim=data.x.shape[1],
+                    hidden_dim=64,
+                    output_dim=output_dim,
+                    num_layers=3,
+                    dropout=0.2)
+    # model = gcn.GCN(input_dim=data.x.shape[1], hidden_dim=128, output_dim=output_dim,
+    #   num_layers=5, dropout=0.2)
+    model.reset_parameters()
+    model.cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    # loss_fn = torch.nn.BCEWithLogitsLoss()
+    loss_fn = FocalLoss(alpha=0.5, gamma=2.0)
+    train_idx = torch.where(data.train_mask)[0]
+    test_idx = torch.where(data.test_mask)[0]
+
+    # get the class prevalences
+    labels_test = data.y[test_idx]
+    class_prevalence = labels_test.sum(0) / len(labels_test)
+    # class_prevalence_test = labels_test[test_idx.cpu()].sum(0) / len(labels_test[test_idx.cpu()])
+
+    print("Class prevalences (train+test):")
+    for i in range(len(class_prevalence)):
+        print(f" {dataset.id_to_loc[i]} {100*class_prevalence[i]:.1f}%")
+
+    acc_all, loss_all = [], []
+    best_acc, best_f1 = 0, 0
+    for i in range(n_steps):
+        loss_scal = train_step(model, data, train_idx, optimizer, loss_fn)
+        if i % 100 == 0:
+            acc_classwise, acc_mean, f1_classwise, f1_mean = eval(
+                model, data, test_idx)
+            acc, f1 = acc_mean, f1_mean
+            best_acc = max(acc, best_acc)
+            best_f1 = max(f1, best_f1)
+            acc_all.append(acc)
+            loss_all.append(loss_scal)
+            print(f"\rCurrent accuracy={100*acc:.1f}%, best_accuracy={100*best_acc:.1f}%, " \
+             f"f1={f1:.2f}, best f1={best_f1:.2f}", end='', flush=True) # print with return carriage
+    print()
+    print(f"Best accuracy {100*best_acc:.1f}%")
+    print()
+
+    print("Baseline accuracy")
+    for i in range(len(class_prevalence)):
+        print(f"{(1-class_prevalence[i]):.2f} ", end='')
+    print()
+    print("actual")
+    for i in range(len(class_prevalence)):
+        print(f"{(acc_classwise[i]):.2f} ", end='')
+    print()
+    print('f1')
+    for i in range(len(class_prevalence)):
+        print(f"{(f1_classwise[i]):.2f} ", end='')
+    ipdb.set_trace()
+
+
+if __name__ == "__main__":
+    model_name = 'linear'
+    
+    for features_type in ['dummy', 'sequencelanguage', 'image']:
+        print(f'model type: ', model_name)
+        print(f"features_type ", features_type)
+        do_training_0(model_name=model_name,
+                      features_type=features_type,
+                      test_split_frac=0.2,
+                      n_steps=10000)
+        print()
+        print()
+
+    # do_training_1(test_split_frac=0.2, n_steps=10000)
+
+    ipdb.set_trace()
+    pass
